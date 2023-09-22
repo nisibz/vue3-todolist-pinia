5d1613b5edc84a6c4ecfa5f032d8504c17f4c8cd
nantachai <bz.nantachai@gmail.com>
2023-09-22 16:42:36 +0700

    connect to rest api

--- src/views/TodoViews.vue
+++ src/views/TodoViews.vue
@@ -1,44 +1,87 @@
 <script setup>
 import { ref, onMounted } from 'vue'
-import { useTodoListStore } from '../stores/todoListItem'
+import axios from 'axios'
+// import { useTodoListStore } from '../stores/todoListItem'
 
-const todoListStore = useTodoListStore()
+// const todoListStore = useTodoListStore()
 
 const lists = ref([])
 const userInput = ref('')
 
-const addTodoList = () => {
-  if (userInput.value !== '') {
-    lists.value.push(userInput.value)
-    userInput.value = ''
+// const addTodoList = () => {
+//   if (userInput.value !== '') {
+//     lists.value.push(userInput.value)
+//     userInput.value = ''
+//   }
+// }
+
+const saveData = () => {
+  // todoListStore.saveData(lists.value)
+}
+
+// const deleteTodoList = (index) => {
+//   lists.value.splice(index, 1)
+// }
+
+const fetchData = async () => {
+  try {
+    const response = await axios.get('http://localhost:3000/api/todolist')
+    lists.value = response.data
+    console.log(lists.value)
+  } catch (error) {
+    console.error('Error fetching data:', error)
   }
 }
 
-const saveData = () => {
-  todoListStore.saveData(lists.value)
+const postData = async () => {
+  try {
+    // Replace with your actual data
+    const data = {
+      name: userInput.value,
+      time: new Date(),
+      success: false
+    }
+    userInput.value = ''
+    const response = await axios.post('http://localhost:3000/api/todolist', data)
+    console.log(response)
+    await fetchData()
+  } catch (error) {
+    console.error('Error posting data:', error)
+  }
 }
 
-const deleteTodoList = (index) => {
-  lists.value.splice(index, 1)
+const deleteData = async (id) => {
+  const response = await axios.delete(`http://localhost:3000/api/todolist/${id}`)
+  console.log(response)
+  await fetchData()
 }
-onMounted(() => {
-  lists.value = todoListStore.lists
+
+const updateTodolist = async (data) => {
+  data.success = !data.success
+  const response = await axios.put(`http://localhost:3000/api/todolist/${data._id}`, data)
+  await fetchData()
+}
+
+onMounted(async () => {
+  await fetchData()
+  // lists.value = todoListStore.lists
 })
 </script>
 
 <template>
   <pre>
-lists value is : {{ lists }}
-userInput value is : {{ userInput }}
+<!-- lists value is : {{ lists }}
+userInput value is : {{ userInput }} -->
 
   </pre>
-  <form @submit.prevent="addTodoList">
+  <form @submit.prevent="postData">
     <input type="text" v-model.trim="userInput" />
     <button>submit</button>
   </form>
   <ul>
     <li v-for="(list, index) of lists" :key="index">
-      {{ list }} <button @click="deleteTodoList(index)">delete</button>
+      <input type="checkbox" v-model="list.success" @click="updateTodolist(list)" />
+      {{ list.name }} {{ list.time }}<button @click="deleteData(list._id)">delete</button>
     </li>
   </ul>
   <button @click="saveData">save</button>
